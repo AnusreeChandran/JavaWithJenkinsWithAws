b7ad38e57582a966a155a58e6aebc2b864319e35
ANUSREE V <anusreechandran99@gmail.com>
2021-09-21 16:27:55 +0530

    initial commit

---
+++ src/main/java/com/spring/AwsSpringBootApp/HelloController.java
@@ -0,0 +1,14 @@
+package com.spring.AwsSpringBootApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/helloOne")
+	public String hello() {
+		return "Hello this is Anusree";
+	}
+
+}
